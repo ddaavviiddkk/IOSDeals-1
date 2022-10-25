6f5e550abd9148db0888829b866c6921d0dbe596
minasedhom1 <minarsedhom@gmail.com>
2022-10-25 16:53:40 -0700

    added login VC
     added login/signup features & connected to Parse

--- Dealicious/Dealicious/AppDelegate.swift
+++ Dealicious/Dealicious/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "wcarbKDBkuI7ZUdAFGx60zNiJifNvt44BHjDPgCG" // <- UPDATE
+                $0.clientKey = "w9Q7sKCeZWZhPtDNcrzXNzllFevpPcXE1BMHC4dw" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
--- Dealicious/Dealicious/LoginViewController.swift
+++ Dealicious/Dealicious/LoginViewController.swift
@@ -1,30 +1,49 @@
 //
-//  LoginViewController.swift
+//  ViewController.swift
 //  Dealicious
 //
-//  Created by Mina Sedhom on 10/25/22.
+//  Created by Mina Sedhom on 10/10/22.
 //  Copyright © 2022 Mina Sedhom. All rights reserved.
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  @IBOutlet weak var usernameField: UITextField!
+        @IBOutlet weak var passwordField: UITextField!
+        
+        
+        @IBAction func onSignIn(_ sender: Any) {
+           
+            PFUser.logInWithUsername(inBackground: usernameField.text! , password: passwordField.text!) { (user, error) in
+                if user != nil {
+                    print(user?.username)
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                } else {
+                    print("Error: \(error?.localizedDescription)")
+                }
+            }
+        }   
+        
+        @IBAction func onSignUp(_ sender: Any) {
+            let user = PFUser()
+            user.username = usernameField.text
+            user.password = passwordField.text
     
+            user.signUpInBackground { (success, error) in
+                if success {
+                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                } else {
+                    print("Error: \(error?.localizedDescription)")
+                }
+            }
+        }
+        override func viewDidLoad() {
+            super.viewDidLoad()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+            // Do any additional setup after loading the view.
+        }
 }
+
